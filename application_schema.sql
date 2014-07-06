f36b501a850660336ca4df4646ebce18796e32a8
bgurung <bgurung.ebox@gmail.com>
2014-07-05 20:59:56 -0500

    Minor fix in the sql script

--- application_schema.sql
+++ application_schema.sql
@@ -1,15 +1,23 @@
-alter table users add column enabled tinyint(1) not null;
-alter table users modify userId username varchar(20);
-alter table users modify username varchar(50);
-alter table users modify email varchar(50);
-alter table users modify password varchar(50);
-alter table users modify display_name varchar(50);
+
+create table users (
+  id bigint(20) not null auto_increment,
+  username varchar(50) not null,
+  email varchar(100) not null,
+  password varchar(50),
+  enabled tinyint(1) not null,
+  display_name varchar(100),
+  primary key(id)
+);
+
+create unique index ix_username on users(username);
+create unique index ix_email on users(email);
 
 create table if not exists authorities (
       username varchar(50) not null,
       authority varchar(50) not null,
       constraint fk_authorities_users foreign key(username) references users(username)
-)
+);
+
 create unique index ix_auth_username on authorities (username,authority);
 
 create table groups (
